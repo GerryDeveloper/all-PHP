b3d770e607950b9d7ce911efc1ea8f42acc3730b
Gerry CR <gerry.dev73@gmail.com>
2025-01-01 03:13:16 -0600

    agregamos la carpeta para realizar una aplicacion de una encuesta usando PDO para manejar la base de datos, y php

---
+++ curso-php-vidaMRR/v35_encuesta/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Encuesta</title>
+</head>
+<body>
+  <form action="#" method="post">
+    <input type="radio" name="lenguaje" id="" value="c">
+    <input type="radio" name="lenguaje" id="" value="c++">
+    <input type="radio" name="lenguaje" id="" value="java">
+    <input type="radio" name="lenguaje" id="" value="swift">
+    <input type="radio" name="lenguaje" id="" value="python">
+    
+    <input type="submit" value="Votar!">
+  </form>
+  
+</body>
+</html>
